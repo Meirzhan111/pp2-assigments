22e53142c0b340d931cdb59c7780ac07b1a12a87
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 21:36:57 +0500

    Add Prac01 topic Casting

---
+++ Prac01/Casting.py
@@ -0,0 +1,13 @@
+#1
+a = int(8)   # a will be 8
+b = int(8.7) # b will be 8
+c = int("9") # c will be 9
+#2
+a = float(1)     # x will be 1.0
+b = float(2.8)   # y will be 2.8
+c = float("3")   # z will be 3.0
+d = float("4.2") # w will be 4.2
+#3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
