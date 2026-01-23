a4bb662ce6ffa62a6e40e2c2aef4bab8e2ae3937
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 19:15:59 +0500

    Add Prac01 topic Syntax

---
+++ Prac01/Syntax.py
@@ -0,0 +1,14 @@
+#1
+if 10 > 7:
+  print("Ten is greater than seven!")
+#2
+if 10 > 7:
+ print("Ten is greater than seven!") 
+if 10 > 7:
+        print("Ten is greater than seven!")
+#3
+x = 8
+y = "Welcome to KBTU!"
+#4
+#I study at KBTU.
+print("Welcome to KBTU!")
