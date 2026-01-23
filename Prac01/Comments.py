ba7ea146acb56d527f515cc09a8127cd772aa05a
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 20:29:25 +0500

    Add Prac01 topic Comments

---
+++ Prac01/Comments.py
@@ -0,0 +1,22 @@
+#1
+#My name is written here
+print("My name is Meirzhan!")
+#2
+print("My name is Meirzhan!") #My name is written here
+#3
+#print("My name is Meirzhan!")
+print("nice to see you!")
+#4
+#This is my comment
+#written in
+#More than one line.
+#More than a single line.
+print("Hello, World!")
+#5
+"""
+This is my comment
+written in
+More than one line.
+More than a single line.
+"""
+print("Good job!")
