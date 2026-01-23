e52498b5f9a14d3791ed1f31d152907d2a1036da
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 19:56:18 +0500

    Add Prac01 topic Statements

---
+++ Prac01/Output.py
@@ -0,0 +1,24 @@
+#Print text
+#1
+print("Hello KBTU!")
+#2
+print("Hello KBTU!")
+print("I am learning Python every day.")
+print("It is wonderful!")
+#3
+print("This is a working method!")
+print('This is also a working method!')
+#4
+print("Hi KBTU!", end=" ")
+print("I’m going to output it on a single line.")
+
+#Print numbers
+#1
+print(0)
+print(785)
+print(65248)
+#2
+print(8 + 20)
+print(4 * 77)
+#3
+print("She is", 26, "years old.")
