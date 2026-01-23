61802d4b032108ae9cd85e3ca71b78cb83d4dbed
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 22:42:42 +0500

    Add Prac01 topic Casting

--- Prac01/Casting.py
+++ Prac01/Casting.py
@@ -11,3 +11,14 @@ d = float("4.2") # w will be 4.2
 x = str("s1") # x will be 's1'
 y = str(2)    # y will be '2'
 z = str(3.0)  # z will be '3.0'
+
+#4
+x = str("hello")  # x will be 'hello'
+y = str(5)        # y will be '5'
+z = str(7.25)     # z will be '7.25'
+
+#5
+a = float(5)       # a will be 5.0
+b = float(6.75)    # b will be 6.75
+c = float("8")     # c will be 8.0
+d = float("9.3")   # d will be 9.3
