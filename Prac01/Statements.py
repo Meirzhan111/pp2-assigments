a4b210d38fb97340d6cf39685b0ce5d5318f6fbe
Meirzhan <ermekkurmangali@mail.ru>
2026-01-23 22:53:39 +0500

    Add Prac01 topic Syntax

--- Prac01/Statements.py
+++
@@ -1,8 +0,0 @@
-#1
-print("Python is very interesting!")
-#2
-print("Welcome to KBTU!")
-print("Have a nice trip.")
-print("I learn Python!")
-#3
-print("Welcome"); print("What is your name?"); print("See you then!")
--- Prac01/Syntax.py
+++ Prac01/Syntax.py
@@ -1,3 +1,4 @@
+#syntax
 #1
 if 10 > 7:
   print("Ten is greater than seven!")
@@ -12,3 +13,13 @@ y = "Welcome to KBTU!"
 #4
 #I study at KBTU.
 print("Welcome to KBTU!")
+
+#statements
+#1
+print("Python is very interesting!")
+#2
+print("Welcome to KBTU!")
+print("Have a nice trip.")
+print("I learn Python!")
+#3
+print("Welcome"); print("What is your name?"); print("See you then!")
