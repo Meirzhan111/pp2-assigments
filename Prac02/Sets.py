69e76bfe14a425fc5d49419f3b6abad4ab60752b
Meirzhan <ermekkurmangali@mail.ru>
2026-01-30 01:04:59 +0500

    Add Prac02 topic Sets

---
+++ Prac02/Sets.py
@@ -0,0 +1,22 @@
+thisset = {"apple", "banana", "cherry", False, True, 0}
+print(thisset)
+
+#1
+Set = {"car","home",45,True}
+print(Set)
+#2
+Set0 = {"anion", "cucumber", "tomato"}
+print("anion" in Set0)
+#3
+fruits_set = {"pear", "grape", "melon"}
+new_fruits = ["apple", "banana"]
+fruits_set.update(new_fruits)
+print(fruits_set)
+#4
+colors = {"red", "blue", "green", "yellow"}
+colors.remove("blue")
+print(colors)
+#5
+shapes = {"circle", "square", "triangle"}
+for shape in shapes:
+    print(shape)
