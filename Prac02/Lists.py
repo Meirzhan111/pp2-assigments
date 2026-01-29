b08da4eb832935afb92402b20669aff6baff6440
Meirzhan <ermekkurmangali@mail.ru>
2026-01-29 23:38:41 +0500

    Add Prac02 topic Lists

---
+++ Prac02/Lists.py
@@ -0,0 +1,21 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#1
+num_word = ["happy",999,"Astana",777]
+print(num_word)
+#2
+Country = ["China","Germany","Russia","Kazakhstan"]
+print(Country[0:2])
+#3
+Animals = ["Dog","Tiger","Monkey"]
+Animals.insert(0, "Bear")
+print(Animals)
+#4
+names = ["Ali", "Aruzhan", "Daniyar"]
+names.pop(2)
+print(names)
+#5
+numbers = [1, 2, 3, 4, 5]
+numbers.sort()
+print(numbers)
