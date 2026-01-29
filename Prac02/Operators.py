79e02b1e257cf2017c705abeb292884e55b1e6ab
Meirzhan <ermekkurmangali@mail.ru>
2026-01-29 23:10:22 +0500

    Add Prac02 topic Operators

---
+++ Prac02/Operators.py
@@ -0,0 +1,26 @@
+print(10 + 5)
+
+#1
+Usd = 1000
+Tg = 10000
+print(Usd+Tg)
+#2
+apple = 50
+banana = 60
+print(apple * banana)
+#3
+a = 10
+b = 2
+if a<b:
+    a//=b
+    print(a)
+else:
+    a*=b
+    print(a)
+#4
+Alice = 25
+Peter = 38
+print(Alice!=Peter)
+#5
+App = ["Teams", "Telegram", "Kaspi.kz"]
+print("TikTok" in App)
