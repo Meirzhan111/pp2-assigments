96ae8baeb58aa83ebdcfca482e8fc6bd30953535
Meirzhan <ermekkurmangali@mail.ru>
2026-01-29 22:36:47 +0500

    Add Prac02 topic Booleans

---
+++ Prac02/Booleans.py
@@ -0,0 +1,21 @@
+print(10 > 9)
+
+#1
+print(20<60)
+#2
+print(999==-999)
+#3
+first = "I am 18 years old"
+second= 1000000
+print(bool(first))
+print(bool(second))
+#4
+elephant_kg = 1000
+lion_kg = 200
+if elephant_kg >lion_kg :
+  print("elephant heavier ")
+else:
+  print(" lion heavier ")
+#5
+print(bool("Kazakhstan independent country"))
+print(bool(999))
