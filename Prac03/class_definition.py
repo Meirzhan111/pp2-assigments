4e4eb4142effe50803c1c02edf5595fa2cbf0015
Meirzhan <ermekkurmangali@mail.ru>
2026-02-06 01:03:01 +0500

    Add Prac02 topic class_definition

---
+++ Prac03/class_definition.py
@@ -0,0 +1,29 @@
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+#1
+class Car:
+    wheels = 4
+my_car = Car()
+print(my_car.wheels)
+#2
+class Person:
+    name = "Alice"
+p = Person()
+print(p.name)
+#3
+class Light:
+    is_on = True
+lamp = Light()
+print(lamp.is_on)
+#4
+class Numbers:
+    values = [1, 2, 3]
+nums = Numbers()
+print(nums.values)
+#5
+class Greeting:
+    message = "Hello!"
+greet = Greeting()
+print(greet.message)
