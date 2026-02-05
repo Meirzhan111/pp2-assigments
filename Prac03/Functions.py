4a64a0f9b1c8b794da6c841f9ed88155413c8177
Meirzhan <ermekkurmangali@mail.ru>
2026-02-05 23:14:37 +0500

    Add Prac02 topic functions

---
+++ Prac03/Functions.py
@@ -0,0 +1,33 @@
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#1
+def new_function():
+    print("Hi from another function")
+
+new_function()
+#2
+def another_function():
+    print("Hello again!")
+another_function()
+another_function()
+another_function()
+#3
+def calculate(number):
+    result = number * 2
+    return result
+a = 10
+b = 25
+print(calculate(a))
+print(calculate(b))
+#4
+def create_text():
+    return "This text comes from a function"
+result = create_text()
+print(result)
+#5
+def make_message():
+    return "This message is created by the function"
+print(make_message())
