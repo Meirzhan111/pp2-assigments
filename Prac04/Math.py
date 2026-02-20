7a58edf78128117e22116b2d681ef57dea258d4d
Meirzhan <ermekkurmangali@mail.ru>
2026-02-20 17:48:09 +0500

    Add Prac02 topic Math

---
+++ Prac04/Math.py
@@ -0,0 +1,25 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+#1
+numbers = (-10,-25,-6)
+for i in numbers:
+    print(abs(i))
+#2
+num = pow(56, 3)
+print(num)
+#3
+import math
+num = math.sqrt(36)
+print(num)
+#4
+import math
+num1 = math.ceil(10.6)
+num2 = math.floor(10.8)
+print(num1)
+print(num2) 
+#5
+import math
+num = math.pi
+print(num)
